87dea1124fdf0c6aabd45607db8d886796ff5930
LujanSolo <lujansolo@proton.me>
2022-10-02 22:17:09 -0700

    updated all handlebars references to model names.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
@@ -33,7 +33,7 @@ router.get('/note/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Note }],
     });
 
     const user = userData.get({ plain: true });
