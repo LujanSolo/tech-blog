1595fd62dc08bf53f75eb465ace237d0a2863994
LujanSolo <lujansolo@proton.me>
2022-10-09 18:25:52 -0700

    Added router.delete to blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -35,4 +35,24 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req,res) => {
+  try {
+    const blogData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      }
+    });
+    
+    if (!blogData) {
+      res.status(404).json({ message: 'No post found with this ID.' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
