4edf8bdf9c18f3745cd64e36962b619ccad7965b
LujanSolo <lujansolo@proton.me>
2022-10-10 19:34:32 -0700

    corrected dashboard routing issues where clicking dashboard took you nowhere. added username to saved data so as to include it on dashboard page as a welcome note

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -16,19 +16,6 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newProject = await Project.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newProject);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
 router.put('/posts/:id', withAuth, async (req, res) => {
   try {
     const post = await Post.update(
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -46,6 +46,26 @@ router.get('/posts/:id', withAuth, async (req, res) => {
   }
 });
 
+//  withAuth middleware to prevent access to route
+router.get('/posts', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('posts', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -54,6 +74,30 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/dashboard', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: { user_id: req.session.user_id },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        }
+      ]
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }))
+
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
