4f1035915e3d40850c00831c5207bb0186726fee
LujanSolo <lujansolo@proton.me>
2022-10-11 20:33:38 -0700

    moved delete function into editpost.js; moved delete button into post.handlebars

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -16,6 +16,26 @@ const editPostHandler = async (event) => {
   });
 };
 
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
 document
   .querySelector('.edit-post-form')
   .addEventListener('submit', editPostHandler);
+
+  document
+    .querySelector('.edit-post-form')
+    .addEventListener('click', delButtonHandler);
