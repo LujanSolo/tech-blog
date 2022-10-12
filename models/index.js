d5c6ea0b445ea28cb18651a79ebea9cdf561889f
LujanSolo <lujansolo@proton.me>
2022-10-11 21:45:43 -0700

    added id ref. to DELETE button in order to differentiate from UPDATE button. Delete button now works properly.

--- models/index.js
+++ models/index.js
@@ -3,27 +3,38 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //*relationship 1-1, 1-many, many-many
-User.hasMany(Post, {
+// User.hasMany(Post, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+//* association (linking)
+Post.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-//* association (linking)
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-});
+// User.hasMany(Comment, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Comment.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
+
+// Comment.belongsTo(Post, {
+//   foreignKey: 'post_id',
+// });
 
 module.exports = { User, Post, Comment };
